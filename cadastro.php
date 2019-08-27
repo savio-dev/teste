4b04400f5c813ef94ec2c2561c06b5a496697390
savio-dev <saviooliveira769@gmail.com>
2019-08-27 15:14:55 -0300

    adição de cpf

--- cadastro.php
+++ cadastro.php
@@ -6,6 +6,7 @@ Nome: <input type="text" name="name"><br>
 Sobrenome: <input type="text" name="lastname"><br>
 E-mail: <input type="text" name="email"><br>
 Telefone: <input type="text" name="telefone"><br>
+cpf: <input type="text" name "cpf"><br>
 <input type="submit">
 </form>
 
