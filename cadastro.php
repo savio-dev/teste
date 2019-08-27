d458d7bcb5c4e87d8ab36e1ff2e42170028ea6b8
savio-dev <saviooliveira769@gmail.com>
2019-08-27 15:46:02 -0300

    exclusão de cpf

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,6 @@ Nome: <input type="text" name="name"><br>
 Sobrenome: <input type="text" name="lastname"><br>
 E-mail: <input type="text" name="email"><br>
 Telefone: <input type="text" name="telefone"><br>
-cpf: <input type="text" name "cpf"><br>
 <input type="submit">
 </form>
 
