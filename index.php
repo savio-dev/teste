fbf11f85fa76cd1360893ed867f9929398ed9926
Elenilda Alves <elenildaalves121@gmail.com>
2019-09-10 14:43:39 -0300

    exercução de cpf

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "myDB";
-
+rrrr
 // Cria conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Checa conexão
