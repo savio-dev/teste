776cfde78c87de0b2b513bbe0a34e9aee0f2ba47
savio-dev <saviooliveira769@gmail.com>
2019-09-11 12:28:21 -0300

    alteração dbname

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "myDB";
+$dbname = "meuBanco";
 
 // Cria conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
